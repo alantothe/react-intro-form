3f9a91a3014ab5e2e100e918cff31a5a37d57bbc
alantothe <rubenamalpartida@gmail.com>
2022-10-28 15:29:08 -0400

    submit

--- react-intro-form/src/App.js
+++ react-intro-form/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
-function App() {
+function App()  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <MovieForm />
+      
+      
     </div>
   );
 }
+function MovieForm(props) {
+  const [title, setTitle] = useState("")
+  const [director, setDirector] = useState("")
+  const [actors, setActors] = useState("")
+  const [plot, setPlot] = useState("")
+  const [imdbRating, setImdbRating] = useState(0)
+  console.log(title)
+  return (
+    <div className="App App-header">
+      <h1>Movie Form</h1>
+      <label>Title:</label>
+      <input type="text" onChange={(event)=>{
+setTitle(event.target.value)
+}}/>
+<p>Current Title: {title}</p>
+
+<label>Director:</label>
+      <input type="text" onChange={(event)=>{
+setDirector(event.target.value)
+}}/>
+<p>Current Director: {director}</p>
+
+<label>Actors:</label>
+      <input type="text" onChange={(event)=>{
+setActors(event.target.value)
+}}/>
+<p>Current Actors: {actors}</p>
+
+<label>Plot:</label>
+      <input type="text" onChange={(event)=>{
+setPlot(event.target.value)
+}}/>
+<p>Current Plot: {plot}</p>
+
+
+<label>IMDB Rating:</label>
+      <input type="number" step="0.1" onChange={(event)=>{
+setImdbRating(event.target.value)
+}}/>
+<p>Current Plot: {imdbRating}</p>
+
+
+
+
+      
+    </div>
+  );
+}
+
 
 export default App;
